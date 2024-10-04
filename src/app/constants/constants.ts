b2464b8a9a8faacd9773856c88073d92dd42d9ed
sanyam710 <e20cse151@bennett.edu.in>
2024-10-05 00:11:59 +0530

    testing deploy.yml

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -4,7 +4,7 @@ export class Constants {
     public static readonly USER_TYPE_STAFF = 3
     public static readonly PUBLISHED = 1;
     public static readonly DRAFT = 2;
-    public static readonly DISCARD = 3;
+    public static readonly DISCARD = 3; 
     public static readonly USER_STATUS = {
         1: "Published",
         2: "Inactive",
